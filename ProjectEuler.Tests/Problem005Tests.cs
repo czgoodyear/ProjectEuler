21731d71e1e1ea52f77673fdb79397a87dacefa0
czgoodyear <czgoodyear@gmail.com>
2025-04-23 14:35:00 -0600

    Solves problem 5

---
+++ ProjectEuler.Tests/Problem005Tests.cs
@@ -0,0 +1,19 @@
+﻿using ProjectEuler.Core.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Tests
+{
+    public class Problem005Tests
+    {
+        [Fact]
+        public void Solve_Returns_CorrectAnswer()
+        {
+            var solver = new Problem005();
+            Assert.Equal("232792560", solver.Solve());
+        }
+    }
+}

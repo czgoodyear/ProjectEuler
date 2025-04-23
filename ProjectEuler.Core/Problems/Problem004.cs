9fc981b83e0cfb056ce82a8830cd22bb507cda5b
czgoodyear <czgoodyear@gmail.com>
2025-04-23 13:42:06 -0600

    Solution to Problem 4

---
+++ ProjectEuler.Core/Problems/Problem004.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Core.Problems
+{
+    [EulerProblem(4, "Largest Palindrome Product")]
+    public class Problem004 : EulerProblemBase
+    {
+        public override int Number => 4;
+        public override string Title => "Largest Palindrome Product";
+
+        public override string Solve()
+        {
+            int largestPalindrome = 0;
+
+            // Iterate downward from 999 to 100 so we find large products first
+            for (int i = 999; i >= 100; i--)
+            {
+                for (int j = i; j >= 100; j--)
+                {
+                    int prod = i * j;
+                    // If product is already less than current max, no need to keep checking smaller j
+                    if (prod <= largestPalindrome)
+                        break;
+
+                    if (IsPalindrome(prod))
+                        largestPalindrome = prod;
+                }
+            }
+
+            return largestPalindrome.ToString();
+        }
+
+        private static bool IsPalindrome(int value)
+        {
+            var s = value.ToString();
+            var rev = new string(s.Reverse().ToArray());
+            return s == rev;
+        }
+    }
+}
